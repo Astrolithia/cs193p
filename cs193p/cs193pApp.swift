62fdb5e2ea2887d21823e536666773e55a03b4d1
Acw <astrolithia@gmail.com>
2025-12-27 15:57:00 +0800

    New Project

---
+++ cs193p/cs193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  cs193pApp.swift
+//  cs193p
+//
+//  Created by Shiro on 12/27/25.
+//
+
+import SwiftUI
+
+@main
+struct cs193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
